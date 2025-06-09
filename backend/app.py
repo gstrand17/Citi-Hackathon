f681e0dbeebd1c7b6bde1ca5689aa6698e790c47
ahmed-lamidi <ahmedlamidi@usf.edu>
2025-06-09 10:05:56 -0400

    first commit

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -6,5 +6,12 @@ app = Flask(__name__)
 def home():
     return jsonify({"message": "Welcome to the Flask API!"})
 
+@app.route('/submit', methods=['POST'])
+def submit():
+    data = request.form.get('name')
+    return f"Received POST request with name: {data}"
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+
