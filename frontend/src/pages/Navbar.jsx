4be82aed35d77f78028564672fdbf688b7af9c86
ana-gilbert <152214588+ana-gilbert@users.noreply.github.com>
2025-06-09 11:13:55 -0400

    fixed navbar styling

--- frontend/src/pages/Navbar.jsx
+++ frontend/src/pages/Navbar.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
-import sprout from '../assets/sprout.png';
-import App from "../App.jsx";
+import sprout from '../assets/images/sprout.png';
+
 const Navbar = () => {
-    return (
-        <nav className="navbar">
-            <p className="nav-title">WealthSprout</p>
-            <image className="nav-logo" src={sprout} alt="logo" />
-            <ul className="nav-list">
-                <li className="nav-item"><Link to="/">Home</Link></li>
-                <li className="nav-item"><Link to="/page1">Comparisons</Link></li>
-                <li className="nav-item"><Link to="/page2">401(K)</Link></li>
-                <li className="nav-item"><Link to="/page3">Roth IRA</Link></li>
-            </ul>
-        </nav>
-    );
+  return (
+    <nav className="navbar">
+      <div className="nav-left">
+        <img className="nav-logo" src={sprout} alt="logo" />
+        <p className="nav-title">WealthSprout</p>
+      </div>
+
+      <ul className="nav-list">
+        <li className="nav-item"><Link to="/">Home</Link></li>
+        <li className="nav-item"><Link to="/page1">Comparisons</Link></li>
+        <li className="nav-item"><Link to="/page2">401(K)</Link></li>
+        <li className="nav-item"><Link to="/page3">Roth IRA</Link></li>
+      </ul>
+    </nav>
+  );
 };
+
 export default Navbar;
